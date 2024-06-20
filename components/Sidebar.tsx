e8653ff1ba4d2fe8b1f6cfd094d03e4e72b1ec29
adg1126 <gutierrez.almiradyan@gmail.com>
2024-06-20 12:00:37 +0800

    Created reusable components with placeholder data

---
+++ components/Sidebar.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { sidebarLinks } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
+
+export default function Sidebar({ user }: SiderbarProps) {
+  const pathname = usePathname();
+
+  return (
+    <section className='sidebar'>
+      <nav className='flex flex-col gap-4'>
+        <Link
+          href='/'
+          className='mb-12 cursor-pointer items-center gap-2 flex'
+        >
+          <Image
+            src='/icons/logo.svg'
+            width={34}
+            height={34}
+            alt='Nexus logo'
+            className='size-[24px] max-xl:size-14'
+          />
+          <h1 className='sidebar-logo'>Nexus</h1>
+        </Link>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathname === link.route || pathname.startsWith(`${link.route}/`);
+
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+            >
+              <div className='relative size-6'>
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  fill
+                  className={cn({ 'brightness-[3] invert-0': isActive })}
+                />
+              </div>
+              <p
+                className={cn('sidebar-label', {
+                  '!text-white': isActive,
+                })}
+              >
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+        {/* user */}
+      </nav>
+      {/* footer */}
+    </section>
+  );
+}
