c1ad46688b2a8f4e9597a90f0c49aeb251fb214e
adg1126 <gutierrez.almiradyan@gmail.com>
2024-06-20 10:17:48 +0800

    Implemented boilerplate for root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Sidebar from '@/components/Sidebar';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Dyan', lastName: 'Gutierrez' };
+
   return (
-    <main>
-      {/* sidebar */}
+    <main className='flex h-screen w-full font-inter'>
+      <Sidebar />
       {children}
     </main>
   );
